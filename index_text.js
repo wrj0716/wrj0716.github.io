d0ce1317d6f7badcc158da610b226160b31861ba
wrj0716 <a0979171072@gmail.com>
2023-12-22 23:21:00 +0800

    12/22

--- index_text.js
+++
@@ -1,8 +0,0 @@
-
-
-function money1(num1){
-     
-    
-    return num1;
-}
-money1(10);
---
+++ js_text.js
@@ -0,0 +1,12 @@
+//var password= 123456;
+//var input;
+//while(password!=input){
+//    input=prompt("請輸入密碼");
+//}
+//alert("登入成功");
+function value1(num1,num2){
+    toto=num1+num2;
+    return toto;
+}
+var mult=value1(10,50);
+document.write(mult);
