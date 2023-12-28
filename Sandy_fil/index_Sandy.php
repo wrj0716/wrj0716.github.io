dfaf8c09f9f00e41d4a3cdceceb758a78dbc0e53
wrj0716 <a0979171072@gmail.com>
2023-12-28 22:01:39 +0800

    12/28

---
+++ Sandy_fil/index_Sandy.php
@@ -0,0 +1,8 @@
+<?php print "hello<br>"; ?>
+<?php print     "hello<br>"; ?>
+<?php print"hello<br>"         ; ?>
+<?php print"hello<br>"
+    ; ?>
+<?php print"hello<br>";?>
+<?php print  "he  l                       l  o<br>  "; ?>
+<?php phpinfo(); ?>
---
+++ Sandy_fil/lesson1.php
@@ -0,0 +1,19 @@
+<?php print "hello<br>"; ?>
+
+<?php print "5+5=10<br>";?>
+
+ 5+5 = <?php print (5+5);?><br>
+ 5+<?php print "5=".(5+5);?><br>
+
+<?php print "This's a book<br>" ?>
+<?php 
+    print strlen("apple")."<br>";
+    print trim( 12112);
+?>
+
+<?php
+
+    $a = strlen("hello");
+    print $a;
+    print strlen("ab");
+?>
